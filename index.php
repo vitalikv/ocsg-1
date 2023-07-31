bba9eaaf74fd84b3d74948863f6cd4f466010942
vit <9455469@mail.ru>
2023-07-31 14:21:28 +0300

    41.1

--- index.php
+++ index.php
@@ -14,7 +14,23 @@
 	<div style="max-width: 1200px; min-width: 400px; margin: 0px auto; font-family: arial,sans-serif; font-size: 18px; color: #666;">	
 		<div style="margin-top: 70px;">
 			1. <a href="/construction">дом</a> 
-		</div>				
+		</div>
+
+		<div style="margin-top: 30px;">
+			2. <a href="/construction?demo=1">гараж</a> 
+		</div>	
+
+		<div style="margin-top: 30px;">
+			3. <a href="/construction?demo=2">од. дом</a> 
+		</div>
+
+		<div style="margin-top: 30px;">
+			4. <a href="/construction?demo=3">дв. дом</a> 
+		</div>
+
+		<div style="margin-top: 30px;">
+			5. <a href="/construction?demo=4">тр. дом</a> 
+		</div>			
 	</div>
 
 
--- p1/index.php
+++ p1/index.php
@@ -8,6 +8,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="shortcut icon" href="/img/favicon.ico" />
 	<title><?=$title?></title>
 	<meta name="description" content="<?=$description?>" />
 	<link rel="stylesheet" href="<?=$path?>css/style.css?<?=$vrs?>"> 
@@ -195,7 +196,7 @@
 	<?}?>
 
 	<? if($_SERVER['SERVER_NAME']=='engineering-plan.ru') {?>
-	<!-- Yandex.Metrika counter --> <script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter40245619 = new Ya.Metrika({ id:40245619, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/40245619" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
+<!-- Yandex.Metrika counter --> <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date(); for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }} k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(40245619, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/40245619" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
 	<?}else{?>
 		<script>
 		console.log('test', window.location.hostname);
